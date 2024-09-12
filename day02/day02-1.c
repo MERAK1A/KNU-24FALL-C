a867ca02b974bec25ad4cd4e69597b1f93384e86
Jeongtak Yang <86291816+MERAK1A@users.noreply.github.com>
2024-09-12 13:31:32 +0900

    day02-1

---
+++ day02/day02-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double num1, num2;
+
+    int option;
+
+    printf("원하는 기능을 선택하시오.\n");
+    printf("1.더하기 2.빼기 3.곱하기 4.나누기\n");
+    scanf("%d", &option);
+
+    printf("숫자를 입력하시오.\n");
+    scanf("%lf %lf", &num1, &num2);
+
+    if (option == 1)
+    {
+        
+
+        printf("%lf + %lf = %lf", num1, num2, num1+num2);
+    }   
+    else if (option == 2)
+    {
+        
+
+        printf("%lf - %lf = %lf", num1, num2, num1 - num2);
+    }
+    else if (option == 3)
+    {
+        
+
+        printf("%lf * %lf = %lf", num1, num2, num1 * num2);
+    }
+    else if (option == 4)
+    {
+        
+
+        printf("%lf / %lf = %lf", num1, num2, num1 / num2);
+    }
+    else
+    {
+        printf("잘못 입력하셨습니다.\n");
+    }
+    
+
+    return 0;
+}
